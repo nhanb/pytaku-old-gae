a22777c867151108e63df8c6319e2d1f57e55dab
nhanb <thanhnhan483@gmail.com>
2014-05-31 19:35:26 +0700

    still something wrong with ember

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -35,8 +35,14 @@ App.RegisterController = Ember.Controller.extend({
 App.Router.map(function() {
     this.resource('login');
     this.resource('register');
-    this.resource('sites');
+    this.resource('sites', function() {
+        this.resource('site', {path: ':site_id'});
+    });
 });
+
+var attr = DS.attr,
+    hasMany = DS.hasMany,
+    belongsTo = DS.belongsTo;
 
 App.SitesRoute = Ember.Route.extend({
     model: function() {
@@ -45,11 +51,36 @@ App.SitesRoute = Ember.Route.extend({
 });
 
 App.Site = DS.Model.extend({
-    domain: DS.attr('string')
+    domain: attr('string'),
+    titles: hasMany('title')
 });
 
-App.Site.FIXTURES = [
-    {id: 1, domain: 'kissmanga.com'},
-    {id: 2, domain: 'mangafox.com'},
-    {id: 3, domain: 'batoto.com'}
-];
+App.Title = DS.Model.extend({
+    name: attr('string'),
+    url: attr('string'),
+    site: belongsTo('site')
+});
+
+App.Title.FIXTURES = [{
+    id: 1,
+    name: 'Naruto',
+    url: 'http://kissmanga.com/Manga/Naruto'
+}, {
+    id: 2,
+    name: 'Tsubasa Reservoir Chronicles',
+    url: 'http://kissmanga.com/Manga/Tsubasa-Reservoir-Chronicles'
+}];
+App.Site.FIXTURES = [{
+    id: 1,
+    domain: 'kissmanga.com',
+    titles: [1, 2]
+}, {
+    id: 2,
+    domain: 'mangafox.com',
+    titles: []
+}, {
+    id: 3,
+    domain: 'batoto.com',
+    titles: []
+}];
+
