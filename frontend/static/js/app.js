fd2926912f8b9cc922de36549d204b3d4d138c42
nhanb <thanhnhan483@gmail.com>
2014-06-01 15:11:20 +0700

    replace "sites" route with empty "search"

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,5 +1,4 @@
 App = Ember.Application.create();
-App.ApplicationAdapter = DS.FixtureAdapter;
 
 App.LoginController = Ember.Controller.extend({
     login: function() {
@@ -35,52 +34,5 @@ App.RegisterController = Ember.Controller.extend({
 App.Router.map(function() {
     this.resource('login');
     this.resource('register');
-    this.resource('sites', function() {
-        this.resource('site', {path: ':site_id'});
-    });
+    this.resource('search');
 });
-
-var attr = DS.attr,
-    hasMany = DS.hasMany,
-    belongsTo = DS.belongsTo;
-
-App.SitesRoute = Ember.Route.extend({
-    model: function() {
-        return this.store.find('site');
-    }
-});
-
-App.Site = DS.Model.extend({
-    domain: attr('string'),
-    titles: hasMany('title')
-});
-
-App.Title = DS.Model.extend({
-    name: attr('string'),
-    url: attr('string'),
-    site: belongsTo('site')
-});
-
-App.Title.FIXTURES = [{
-    id: 1,
-    name: 'Naruto',
-    url: 'http://kissmanga.com/Manga/Naruto'
-}, {
-    id: 2,
-    name: 'Tsubasa Reservoir Chronicles',
-    url: 'http://kissmanga.com/Manga/Tsubasa-Reservoir-Chronicles'
-}];
-App.Site.FIXTURES = [{
-    id: 1,
-    domain: 'kissmanga.com',
-    titles: [1, 2]
-}, {
-    id: 2,
-    domain: 'mangafox.com',
-    titles: []
-}, {
-    id: 3,
-    domain: 'batoto.com',
-    titles: []
-}];
-
