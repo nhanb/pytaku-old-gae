6abcb70ef3ac2e3f7f37d66eea923acedae7578c
nhanb <thanhnhan483@gmail.com>
2014-06-01 21:04:37 +0700

    frontend facelift

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -3,6 +3,8 @@ App = Ember.Application.create();
 App.LoginController = Ember.Controller.extend({
     login: function() {
         var self = this;
+        self.set('errorMessage', null);
+
         var data = this.getProperties('email', 'password');
         $.post('/api/auth', JSON.stringify(data)).then(function(resp) {
             if (resp.success) {
@@ -17,6 +19,13 @@ App.LoginController = Ember.Controller.extend({
 });
 
 App.RegisterController = Ember.Controller.extend({
+    reset: function() {
+        this.setProperties({
+            email: '',
+            password: '',
+            errorMessage: ''
+        });
+    },
     register: function() {
         var self = this;
         var data = this.getProperties('email', 'password');
@@ -34,5 +43,19 @@ App.RegisterController = Ember.Controller.extend({
 App.Router.map(function() {
     this.resource('login');
     this.resource('register');
-    this.resource('search');
+    this.resource('search', function() {
+        this.resource('result', {path: ':query'})
+    });
+});
+
+App.LoginRoute = Ember.Route.extend({
+    setupController: function(controller, context) {
+        controller.reset();
+    }
+});
+
+App.ResultRoute = Ember.Route.extend({
+    model: function(params) {
+        return $.getJSON('/api/search?keyword=' + encodeURIComponent(params.query));
+    }
 });
