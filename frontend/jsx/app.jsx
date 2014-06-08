e3a7f1d96474c87cdf6ef0b1f03de200b9d9981b
nhanb <thanhnhan483@gmail.com>
2014-06-08 20:28:37 +0700

    cleaner routing, allow viewing frontend-dist in NERDTree

--- frontend/jsx/app.jsx
+++ frontend/jsx/app.jsx
@@ -3,12 +3,8 @@ $(document).ready(function() {
     var routed = document.getElementById('routed');
 
     var routes = {
-        '/': function() {
-            React.renderComponent(<Home />, routed);
-        },
-        '/register': function() {
-            React.renderComponent(<Register /> , routed);
-        }
+        '/': function() {React.renderComponent(<Home />, routed);},
+        '/register': function() {React.renderComponent(<Register /> , routed);}
     };
     var routerHandler = new Router(routes);
     routerHandler.init('/');
