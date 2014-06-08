a927b9496fab0c5482086cfa0b0a78f63593a813
nhanb <thanhnhan483@gmail.com>
2014-06-08 16:57:41 +0700

    use director.js, add agignore

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,6 +12,12 @@ gulp.task('jsx', function() {
     .pipe(gulp.dest('frontend-dist/static'));
 });
 
+gulp.task('jslib', function() {
+    gulp.src('frontend/lib/**/*.js')
+    .pipe(concat('lib.js'))
+    .pipe(gulp.dest('frontend-dist/static'));
+});
+
 gulp.task('css', function() {
     gulp.src('frontend/css/**/*.css')
     .pipe(concat('main.css'))
@@ -26,12 +32,13 @@ gulp.task('html', function() {
 });
 
 gulp.task('default', [], function() {
-    gulp.start('jsx', 'css', 'html');
+    gulp.start('jsx', 'jslib', 'css', 'html');
 });
 
 gulp.task('watch', function() {
     gulp.watch('frontend/css/**/*.css', ['css']);
     gulp.watch('frontend/jsx/**/*.jsx', ['jsx']);
+    gulp.watch('frontend/lib/**/*.js', ['jslib']);
     gulp.watch('frontend/app.html', ['html']);
 
     var server = livereload();
