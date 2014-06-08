2f4033b5905be55c8d20afe0575cd493b784fd5e
nhanb <thanhnhan483@gmail.com>
2014-06-08 16:34:50 +0700

    setup react and gulp

---
+++ Gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var react = require('gulp-react');
+var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var minifycss = require('gulp-minify-css');
+
+gulp.task('jsx', function() {
+    gulp.src('frontend/jsx/**/*.jsx')
+    .pipe(react())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('frontend-dist/static'));
+});
+
+gulp.task('css', function() {
+    gulp.src('frontend/css/**/*.css')
+    .pipe(concat('main.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('frontend-dist/static'));
+});
+
+gulp.task('html', function() {
+    gulp.src('frontend/app.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('frontend-dist'));
+});
+
+gulp.task('default', [], function() {
+    gulp.start('jsx', 'css', 'html');
+});
+
+gulp.task('watch', function() {
+    gulp.watch('frontend/css/**/*.css', ['css']);
+    gulp.watch('frontend/jsx/**/*.jsx', ['jsx']);
+    gulp.watch('frontend/app.html', ['html']);
+
+    var server = livereload();
+    gulp.watch(['frontend-dist/**', 'frontend']).on('change', function(file) {
+        server.changed(file.path);
+    });
+});
