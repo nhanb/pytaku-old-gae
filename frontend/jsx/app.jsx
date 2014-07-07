e81b80498f4f3e9dbac1bcbf7a5415ce5dc26579
nhanb <nhan@nerdyweekly.com>
2014-07-07 21:50:35 +0700

    Refactor app.jsx to be One True (Owner) Component
    Beware: somehow old routes seem not to be garbage collected. For
    example: using Prev/Next seems to add memory usage, which most likely
    indicates that the old chapter is not discarded. Gotta look into this.

--- frontend/jsx/app.jsx
+++ frontend/jsx/app.jsx
@@ -1,27 +1,88 @@
 /** @jsx React.DOM */
-$(document).ready(function() {
 
-    // Render navbar
-    var navbar = document.getElementById('navbar');
-    React.renderComponent(<Navbar />, navbar);
+var app = {
+    HOME: 'home',
+    REGISTER: 'register',
+    LOGIN: 'login',
+    SEARCH: 'search',
+    TITLE: 'title',
+    CHAPTER: 'chapter'
+};
 
-    // Render main content
-    var routed = document.getElementById('routed');
-    var routes = {
-        '/': function() {React.renderComponent(<Home />, routed);},
-        '/register': function() {React.renderComponent(<Register />, routed);},
-        '/login': function() {React.renderComponent(<Login />, routed);},
-        '/search': function() {React.renderComponent(<Search />, routed);},
-        '/search/(.+)': function(query) {
-            React.renderComponent(<Search query={query} />, routed);
-        },
-        '/title/(.+)': function(url) {
-            React.renderComponent(<Title url={url} />, routed);
-        },
-        '/chapter/(.+)': function(url) {
-            React.renderComponent(<Chapter url={url} />, routed);
+var PytakuApp = React.createClass({
+    componentDidMount: function() {
+        var setState = this.setState;
+        var self = this;
+        var router = Router({
+            '/': setState.bind(this, {route: app.HOME}),
+            '/register': setState.bind(this, {route: app.REGISTER}),
+            '/login': setState.bind(this, {route: app.LOGIN}),
+            '/search': setState.bind(this, {route: app.SEARCH}),
+
+            '/search/(.+)': (function() {
+                return function(query) {
+                    self.setState({
+                        route: app.SEARCH,
+                        query: query
+                    });
+                };
+            })(),
+
+            '/title/(.+)': (function() {
+                return function(query) {
+                    self.setState({
+                        route: app.TITLE,
+                        url: query
+                    });
+                };
+            })(),
+
+            '/chapter/(.+)': (function() {
+                return function(query) {
+                    self.setState({
+                        route: app.CHAPTER,
+                        url: query
+                    });
+                };
+            })(),
+        });
+        router.init('/');
+    },
+
+    getInitialState: function() {
+        return {
+            route: app.HOME
+        };
+    },
+
+    render: function() {
+        var routeComponent;
+        switch (this.state.route) {
+            case app.REGISTER:
+                routeComponent = <Register />;
+                break;
+            case app.LOGIN:
+                routeComponent = <Login />;
+                break;
+            case app.SEARCH:
+                routeComponent = <Search query={this.state.query}/>;
+                break;
+            case app.TITLE:
+                routeComponent = <Title url={this.state.url}/>;
+                break;
+            case app.CHAPTER:
+                routeComponent = <Chapter url={this.state.url}/>;
+                break;
+            default:
+                routeComponent = <Home />;
         }
-    };
-    var routerHandler = new Router(routes);
-    routerHandler.init('/');
+        return (
+            <div>
+                <Navbar />
+                {routeComponent}
+            </div>
+        );
+    }
 });
+
+React.renderComponent(<PytakuApp />, document.getElementById('app'));
