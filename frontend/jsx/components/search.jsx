3f837de5221a92898d9d9f3086e563dfc4b62ab3
nhanb <nhan@nerdyweekly.com>
2014-07-08 11:20:31 +0700

    don't require loggin in for <Search> anymore

--- frontend/jsx/components/search.jsx
+++ frontend/jsx/components/search.jsx
@@ -113,7 +113,7 @@ var SearchButton = React.createClass({
 });
 
 var Search = React.createClass({
-    mixins: [RouteMixin, RequireLoginMixin],
+    mixins: [RouteMixin],
     pageTitle: 'Search manga title',
 
     getInitialState: function() {
