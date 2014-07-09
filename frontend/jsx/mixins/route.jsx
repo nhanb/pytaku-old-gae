e10e95c1ded2d959ff0b7711431bb9f37c81864e
nhanb <nhan@nerdyweekly.com>
2014-07-09 07:55:53 +0700

    put updateTitle inside a closure
    This avoids polluting global namespace

--- frontend/jsx/mixins/route.jsx
+++ frontend/jsx/mixins/route.jsx
@@ -1,20 +1,26 @@
 // Update title for each route
-var updateTitle = function() {
-    var title;
-    switch (typeof(this.pageTitle)) {
-        case 'string':
-            title = this.pageTitle + ' | Pytaku';
-        break;
-        case 'function':
-            title = this.pageTitle() + ' | Pytaku';
-        break;
-        default:
-            title = "Pytaku - The last manga reader page you'll ever need";
-    }
-    document.title = title;
-};
+var RouteMixin;
 
-var RouteMixin = {
-    componentDidUpdate: updateTitle,
-    componentDidMount: updateTitle
-};
+(function(){
+    // Closure to avoid polluting global namespace with updateTitle.
+    // TODO: I should probably consider a real solution like CommonJS or AMD...
+    var updateTitle = function() {
+        var title;
+        switch (typeof(this.pageTitle)) {
+            case 'string':
+                title = this.pageTitle + ' | Pytaku';
+            break;
+            case 'function':
+                title = this.pageTitle() + ' | Pytaku';
+            break;
+            default:
+                title = "Pytaku - The last manga reader page you'll ever need";
+        }
+        document.title = title;
+    };
+
+    RouteMixin = {
+        componentDidUpdate: updateTitle,
+        componentDidMount: updateTitle
+    };
+})();
