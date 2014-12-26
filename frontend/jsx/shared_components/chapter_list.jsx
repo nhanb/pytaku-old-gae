7490e22d6f2f13a0f2d20b77bc967941048d6fa3
nhanb <nhan@nerdyweekly.com>
2014-12-27 00:59:17 +0700

    ensure unique key in ChapterList component
    Some stupid sites have duplicate chapters. Nothing much we can do about
    it...
    
    For example: http://vechai.info/one-piece-digital-colored-comics/
    There's a duplicate "chapter 17" where "chapter 27" is supposed to be.

--- frontend/jsx/shared_components/chapter_list.jsx
+++ frontend/jsx/shared_components/chapter_list.jsx
@@ -4,7 +4,7 @@ echo = require('../language.jsx').echo;
 
 module.exports = React.createClass({
 
-    renderChapter: function(chapter) {
+    renderChapter: function(chapter, index) {
         var href = '/#/chapter/' + encodeURIComponent(chapter.url);
         var progress = '';
         if (chapter.progress === 'finished') {
@@ -13,7 +13,7 @@ module.exports = React.createClass({
             progress = <span className="badge alert-info">{echo('reading')}</span>;
         }
         return (
-            <a href={href} key={chapter.url} className="list-group-item">
+            <a href={href} key={chapter.url + index} className="list-group-item">
                 {chapter.name} {progress}
             </a>
         );
