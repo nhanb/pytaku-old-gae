d86be073d8ecf02c685699250da740682b2a276a
nhanb <nhan@nerdyweekly.com>
2014-12-28 21:47:44 +0700

    more detailed home page

--- frontend/jsx/routes/home.jsx
+++ frontend/jsx/routes/home.jsx
@@ -6,7 +6,13 @@ module.exports = React.createClass({
     render: function() {
         return (
             <div className="container">
-                <h2>Welcome to Pytaku!</h2>
+                <h2>{echo('welcome_to_pytaku')}</h2>
+                <p>{echo('pytaku_is')}</p>
+                <p>{echo('pytaku_instructions')}</p>
+                <p>{echo('can_be_found_on_github')}:</p>
+                <a href="https://github.com/nhanb/pytaku" className="external">
+                    <i className="fa fa-lg fa-github"></i> https://github.com/nhanb/pytaku
+                </a>
             </div>
         );
     }
